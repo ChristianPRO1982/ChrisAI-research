7345de6557c3b25ff2b3b57f2bc292bc58b865d3
ChristianPRO1982 <christian.martiny@outlook.com>
2025-01-16 12:59:10 +0100

    sdf

--- src/main.py
+++ src/main.py
@@ -21,13 +21,13 @@ FOLDER_PATH = os.getenv("FOLDER_PATH")
 PREFIX = os.getenv("PREFIX")
 
 if utils.init():
-    logging_msg("utils.extract_hal_gen START")
+    logging_msg("utils.extract_hal_gen START", 'WARNING')
     stop_and_go = utils.extract_hal_gen()
     if stop_and_go:
-        logging_msg("utils.extract_hal START")
+        logging_msg("utils.extract_hal START", 'WARNING')
         stop_and_go = utils.extract_hal()
     if stop_and_go:
-        logging_msg("utils.extract_stat START")
+        logging_msg("utils.extract_stat START", 'WARNING')
         stop_and_go = utils.extract_stat()
 
 logging_msg("END PROGRAM", "WARNING")
